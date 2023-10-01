f62fa6f1d02d9cfcaf66fabe9ef695253f2b14c1
Thomas <thomastayac@gmail.com>
2023-10-01 13:22:46 -0700

    test

--- pkg/varint/varint_test.go
+++ pkg/varint/varint_test.go
@@ -6,8 +6,26 @@ import (
 	"github.com/thomastay/rash-db/pkg/varint"
 )
 
-func TestVarIntConsecutive(t *testing.T) {
-	for i := uint64(0); i < 65536; i++ {
-		varint.Encode64(i)
+func TestOneByteVarInt(t *testing.T) {
+	for i := uint64(0); i <= 240; i++ {
+		b := varint.Encode64(i)
+		if len(b) != 1 {
+			t.Errorf("%d: Length of b should be 1, got %d", i, len(b))
+		}
+	}
+}
+
+func TestTwoByteVarInt(t *testing.T) {
+	seen := make(map[uint16]bool)
+	for i := uint64(241); i <= 2287; i++ {
+		b := varint.Encode64(i)
+		if len(b) != 2 {
+			t.Errorf("%d: Length of b should be 2, got %d", i, len(b))
+		}
+		key := uint16(b[0])*256 + uint16(b[1])
+		if _, ok := seen[key]; ok {
+			t.Errorf("%d: Duplicated key, %d, %d", i, b[0], b[1])
+		}
+		seen[key] = true
 	}
 }
